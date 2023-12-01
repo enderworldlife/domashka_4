c184d477f533b7eb79aba53ffde8c7d621ae71d3
Your Name <you@example.com>
2023-12-01 17:12:30 +0300

    zadanye 3

--- Program.cs
+++ Program.cs
@@ -12,21 +12,28 @@
 
 // zadanye 2
 
-Random random = new Random();
-int[] massiv = new int[] {222, 542, 531, 890, 387};
+// Random random = new Random();
+// int[] massiv = new int[] {222, 542, 531, 890, 387};
 
-Console.Write("Chisla: ");
-for (int j = 0; j < massiv.Length; j++){
-    if (massiv[j] % 2 == 0) Console.Write(massiv[j] + " ");
+// Console.Write("Chisla: ");
+// for (int j = 0; j < massiv.Length; j++){
+//     if (massiv[j] % 2 == 0) Console.Write(massiv[j] + " ");
+// }
+// Console.WriteLine();
+// Console.Write("Chetnye chisla: ");
+
+// for (int i = 0; i < massiv.Length; i++){
+//     if (massiv[i] % 2 == 0) Console.Write(massiv[i] + " ");
+// }
+
+// zadanye 3
+
+int[] massiv = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int i = massiv.Length - 1;
+while (i >= 0){
+    Console.Write(massiv[i] + " ");
+    i--;
 }
-Console.WriteLine();
-Console.Write("Chetnye chisla: ");
-
-for (int i = 0; i < massiv.Length; i++){
-    if (massiv[i] % 2 == 0) Console.Write(massiv[i] + " ");
-}
-
-
 
 
 
