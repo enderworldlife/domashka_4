6c56dba3b3666a4d581c26463e15aa4891b9ac6f
Your Name <you@example.com>
2023-12-01 16:52:41 +0300

    zadanye 2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
 ﻿
 // zadanye 1
 
-bool run = true;
-while (run == true){
-    Console.Write("Input: ");
-    string input = Console.ReadLine();
-    if (input == "q"){
-        run = false;
-    }
+// bool run = true;
+// while (run == true){
+//     Console.Write("Input: ");
+//     string input = Console.ReadLine();
+//     if (input == "q"){
+//         run = false;
+//     }
+// }
+
+// zadanye 2
+
+Random random = new Random();
+int[] massiv = new int[] {222, 542, 531, 890, 387};
+
+Console.Write("Chisla: ");
+for (int j = 0; j < massiv.Length; j++){
+    if (massiv[j] % 2 == 0) Console.Write(massiv[j] + " ");
 }
+Console.WriteLine();
+Console.Write("Chetnye chisla: ");
 
-
+for (int i = 0; i < massiv.Length; i++){
+    if (massiv[i] % 2 == 0) Console.Write(massiv[i] + " ");
+}
 
 
 
